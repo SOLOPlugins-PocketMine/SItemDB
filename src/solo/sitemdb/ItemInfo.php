ce20723eac4776d352118bf1dc2398922ec8194f
solo5star <solo_5@naver.com>
2017-08-21 22:43:44 +0900

    Add files via upload

---
+++ src/solo/sitemdb/ItemInfo.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace solo\sitemdb;
+
+use pocketmine\item\Item;
+
+class ItemInfo implements \JsonSerializable{
+
+  protected $name;
+
+  protected $item;
+
+  protected $description;
+
+  public function __construct(Item $item, string $customName = null, string $description = null){
+    if($customName !== null){
+      $this->name = $customName;
+    }else if($item->hasCustomName()){
+      $this->name = $item->getCustomName();
+    }else{
+      $this->name = $item->getName();
+    }
+    $item->setCount(1);
+    $this->item = clone $item;
+    $this->description = $description ?? "";
+  }
+
+  public function getName() : string{
+    return $this->name;
+  }
+
+  public function getItem() : Item{
+    return $this->item;
+  }
+
+  public function getDescription() : string{
+    return $this->description;
+  }
+
+  public function jsonSerialize(){
+    return [
+      "item" => $this->item,
+      "name" => $this->name,
+      "description" => $this->description
+    ];
+  }
+
+  public static function jsonDeserialize(array $data){
+    $id = intval($data["item"]["id"]);
+    $damage = intval($data["item"]["damage"]);
+    $count = intval($data["item"]["count"]);
+    $nbt = "";
+    if(isset($data["item"]["nbt"])){
+      $nbt = $data["item"]["nbt"];
+    }else if(isset($data["item"]["nbt_hex"])){
+      $nbt = hex2bin($data["item"]["nbt_hex"]); // added in API 3.0.0-ALPHA7
+    }
+    $item = Item::get($id, $damage, $count, $nbt);
+    return new ItemInfo(
+      $item,
+      $data["name"],
+      $data["description"]
+    );
+  }
+}
